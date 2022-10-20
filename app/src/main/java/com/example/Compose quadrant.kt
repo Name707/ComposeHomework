1100d3fea0222138653d4649d23e0dd7ddaedffc
Name707 <707VLAN@gmail.com>
2022-10-20 21:25:43 +0600

    Commit2

--- app/src/main/java/com/example/Compose quadrant.kt
+++ app/src/main/java/com/example/Compose quadrant.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
@@ -102,7 +103,7 @@ fun ComposeCard(
          modifier = Modifier.padding(bottom = 16.dp),
          fontWeight = FontWeight.Bold,
          style = TextStyle(
-color = androidx.compose.ui.graphics.Color.Blue
+         color = Cyan
          ),
 
      )
