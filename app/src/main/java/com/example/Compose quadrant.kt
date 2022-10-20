d4524068ff619a3fe8ccde63b6ab0148ef9d5ba5
Name707 <707VLAN@gmail.com>
2022-10-20 21:17:00 +0600

    Answer

--- app/src/main/java/com/example/Compose quadrant.kt
+++ app/src/main/java/com/example/Compose quadrant.kt
@@ -1,17 +1,29 @@
 package com.example
 
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,8 +38,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeHomeworkTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    ComposeQuadrant(firstText = "1", secondText = "2", thirdText = "3", fouthText = "4")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    ComposeQuadrant()
+
 
                 }
             }
@@ -35,36 +48,70 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun ComposeQuadrant(firstText: String, secondText: String, thirdText: String, fouthText: String) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight(),
-  ) {
-        Text(
-            text = firstText,
-
+fun ComposeQuadrant() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposeCard(
+                title = "Title1",
+                description = "main text",
+                modifier = Modifier.weight(1f),
             )
-        Text(
-            text = secondText
-        )
-    }
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight())  {
+            ComposeCard(
+                title = "Title2",
+                description = "main text",
+                modifier = Modifier.weight(1f),
 
-        Text(
-            text = thirdText
-        )
-        Text(
-            text = fouthText
-        )
-    }
-    }
+                )
+        }
+            Row(Modifier.weight(1f)) {
+                ComposeCard(
+                    title = "Title3",
+                    description = "main text",
+                    modifier = Modifier.weight(1f),
+                )
+                ComposeCard(
+                        title = "Title4",
+                        description = "main text",
+                        modifier = Modifier.weight(1f),
+                )
 
+            }
+            }
+        }
+
+
+@Composable
+fun ComposeCard(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier,
+    )
+{
+ Column(
+     modifier = modifier
+         .fillMaxSize()
+         .padding(16.dp),
+     horizontalAlignment = Alignment.CenterHorizontally,
+     verticalArrangement = Arrangement.Center,
+
+      )
+ {
+     Text(
+         text = title,
+         modifier = Modifier.padding(bottom = 16.dp),
+         fontWeight = FontWeight.Bold,
+     )
+     Text(
+         text = description,
+         textAlign = TextAlign.Justify,
+     )
+ }
+
+}
 @Preview(showBackground = true)
 @Composable
-fun ComposeArticlePreview() {
+fun ComposeQuadrantPreview() {
     ComposeHomeworkTheme {
-ComposeQuadrant ("1", "2", "3", "4")
+ComposeQuadrant ()
     }
 }
